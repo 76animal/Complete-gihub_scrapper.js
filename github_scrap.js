aa20fc457d0540383ad6f2c6fc18ef9bdd68f780
76animal <51908662+76animal@users.noreply.github.com>
2021-08-16 01:50:28 +0530

    Update github_scrap.js

--- github_scrap.js
+++ github_scrap.js
@@ -9,7 +9,7 @@ async function getProjectURLs(url,i)
 {
     let browser = new wd.Builder().forBrowser('chrome').build();
     await browser.get(url);
-    await browser.wait(wd.until.elementLocated(wd.By.css("a.text-bold")));  // safety condition sare projectboxes jab tak nikal na lo tab tak broswer ko bolo wait kre
+    await browser.wait(wd.until.elementsLocated(wd.By.css("a.text-bold")));  // safety condition sare projectboxes jab tak nikal na lo tab tak broswer ko bolo wait kre
     let projectBoxes = await browser.findElements(wd.By.css("a.text-bold")); // sare project nikal la each  topic ke
      totalProjects += ((projectBoxes.length>8) ? 8 : projectBoxes.length);
     finalData[i]["projects"] = [];
@@ -19,35 +19,49 @@ async function getProjectURLs(url,i)
         {
             break;
         }
-        finalData[i].projects.push({projectURL : await projectBoxes[j].getAttribute("href")});
+        finalData[i].projects.push({"projectURL" : await projectBoxes[j].getAttribute("href")});
     }
+    
     let projects = finalData[i]["projects"];
     for(let k in projects)
     {
-        getIssues(projects[k].projectURL,i,j);
+        getIssues(projects[k].projectURL,i,k);
     }
     browser.close();
 }
 
 async function getIssues(url,i,j)
 {
-    let broswer = new wd.Builder().forBrowser('chrome').build();
-    await browser.get(url + "/issues");
-    finalData[i].projects[j]["issues"] = [];
-    let issueBoxes = await browser.findElements(wd.By.css(".Link--primary.v-align-middle.no-underline.h4.js-navigation-open.markdown-title"));
+    let browser = new wd.Builder().forBrowser('chrome').build();
+    await browser.get( url + "/issues");
     let currUrl = await browser.getCurrentUrl();
-    if(currUrl == (url + "/issues") && issueBoxes.length!=0)
+    finalData[i].projects[j]["issues"] = [];
+    if( currUrl.localeCompare(url + "/issues") != 0 )
     {
-        for(let l in issueBoxes)
+        browser.close();
+        finalData[i].projects[j]["issues"][0] = "No issues found";
+        fs.writeFileSync("finalData.json", JSON.stringify(finalData));
+        return;
+    }
+    // await browser.wait(wd.until.elementsLocated(wd.By.css(".v-align-middle.no-underline"))); 
+    let issueBoxes = await browser.findElements(wd.By.css(".v-align-middle.no-underline"));
+    
+    if(issueBoxes.length == 0)
+    {
+        browser.close();
+        finalData[i].projects[j]["issues"][0] = "No issues found";
+        fs.writeFileSync("finalData.json", JSON.stringify(finalData));
+        return;
+    }
+    for(let l in issueBoxes)
+    {
+        if(l==8)
         {
-            if(l==8)
-            {
-                break;
-            }
-            let heading = await issueBoxes[l].getAttribute("innertext");
-            let url = await issueBoxes[l].getAttribute("href");
-            finalData[i].projects[j].issues.push({"heading":heading,"url":url});
+            break;
         }
+        let heading = await issueBoxes[l].getAttribute("innerText");
+        let issueURL = await issueBoxes[l].getAttribute("href");
+        finalData[i].projects[j].issues.push({"heading":heading,"url":issueURL});
     }
 
     projectscovered += 1;
@@ -61,14 +75,14 @@ async function getIssues(url,i,j)
 async function main(){
     await browser.get(`https://github.com/topics`);
 
-    await browser.wait(wd.until.elementLocated(wd.By.css(".no-underline.d-flex.flex-column.flex-justify-center")));
-    let topicBoxes = await browser.findElements(wd.By.css(".no-underline.d-flex.flex-column.flex-justify-center"));
-
+    await browser.wait(wd.until.elementsLocated(wd.By.css(".no-underline.d-flex.flex-column")));
+    let topicBoxes = await browser.findElements(wd.By.css(".no-underline.d-flex.flex-column"));
+    
     for(let topicBox of topicBoxes)
     {
-        finalData.push({"topicURL": topicBox.getAttribute("href")});
+        finalData.push({ topicURL : await topicBox.getAttribute("href")});
     }
-
+    
     for(let i in finalData)
     {
         getProjectURLs(finalData[i].topicURL,i);
